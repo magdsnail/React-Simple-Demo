ccc7d0c34c3b1bc642a9496501aac4306edff5ef
jason.wang <wangkjim@gmail.com>
2018-10-16 20:47:22 +0800

    .

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
     constructor(props) {
@@ -12,13 +13,24 @@ class TodoItem extends Component {
     }
     
     render() {
-        const { content } = this.props;
+        const { content, test } = this.props;
         return (
             <div onClick={this.handleClick}>
-                {content}
+                {test} - {content}
             </div>
         );
     }
+}
+
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+    test: 'hello world'
 }
 
 export default TodoItem;
